aac6e1b6c9485de8e230c13674e11a7a15b97043
Ichlasul Amal <13511075@std.stei.itb.ac.id>
2014-03-27 17:45:50 +0700

    Update point

--- src/header/Point.hpp
+++ src/header/Point.hpp
@@ -7,7 +7,35 @@ public:
 	Point(int x, int y) : x(x), y(y){};
 	~Point(){};
 
-public:
+	int getX() {
+		return x;
+	}
+
+	int getY() {
+		return y;
+	}
+
+	bool isOutOfBound() {
+		return x >= 0 && x < 640 && y >= 0 && y < 480;
+	}
+
+	Point up() {
+		return new Point(x, y - 1);
+	}
+
+	Point down() {
+		return new Point(x, y + 1);
+	}
+
+	Point left() {
+		return new Point(x - 1, y);
+	}
+
+	Point right() {
+		return new Point(x + 1, y);
+	}
+
+private:
 	int x;
 	int y;
 };
