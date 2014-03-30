6918ebb821e261caa0029ab1d53bdcd661ad11fe
Ichlasul Amal <13511075@std.stei.itb.ac.id>
2014-03-31 02:57:45 +0700

    Add car'

--- src/header/Car.hpp
+++ src/header/Car.hpp
@@ -1,10 +1,31 @@
 #ifndef GRAFIKA_CAR_HPP
 #define GRAFIKA_CAR_HPP
 
-class Car {
-public:
-	Car() {};
-	~Car() {};
-	
+#include "src/header/Polygon.hpp"
+
+class Car : public Polygon {
+
+public:	
+	Car();
+	~Car();
+	Car(int, int, int, int);
+	void draw();
+	void moveLeft();
+	void moveRight();
+	int getPosX();
+	int getPosY();
+	int getPanjang();
+	int getLebar();
+	void setPosX(int);
+	void setPosY(int);
+	void setPanjang(int);
+	void setLebar(int);
+
+
+private:
+	int posX;
+	int posY;
+	int panjang;
+	int lebar;
 };
 #endif
