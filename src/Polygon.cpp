358159883d8dd2043bd9846e94539cce4bddb662
Ichlasul Amal <13511075@std.stei.itb.ac.id>
2014-03-30 22:02:17 +0700

    Add polygon

---
+++ src/Polygon.cpp
@@ -0,0 +1,22 @@
+#include "src/header/Polygon.hpp"
+
+Polygon::Polygon() {
+
+}
+Polygon::~Polygon() {
+
+}
+
+void Polygon::drawLine() {
+
+}
+
+void Polygon::fillPoly() {
+
+}
+
+void Polygon::translate(int x, int y) {}
+
+void Polygon::rotate(int theta) {}
+
+void Polygon::scale(int width, int height) {}
--- src/header/Polygon.hpp
+++ src/header/Polygon.hpp
@@ -1,11 +1,22 @@
 #ifndef GRAFIKA_POLYGON_HPP
 #define GRAFIKA_POLYGON_HPP
 
-class Polygon {
+#include "src/header/Plane.hpp"
+
+class Polygon : public Plane {
 public:
-	Polygon() {};
-	~Polygon() {};
-	 
+	Polygon();
+	~Polygon();
+
+	void fillPoly();
+	void drawLine();
+	void translate(int x, int y);
+	void rotate(int theta);
+	void scale(int width, int height);
+
+private:
+	int * vertices;
+	int num;
 };
 
 #endif
