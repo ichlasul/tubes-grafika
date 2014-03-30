44d8e9b39de4b21948b1e4a67cf9776fc57931c3
Ichlasul Amal <13511075@std.stei.itb.ac.id>
2014-03-30 19:28:46 +0700

    Add bg

---
+++ src/Bground.cpp
@@ -0,0 +1,9 @@
+#include "Bground.h"
+
+Bground::Bground() {
+	horizon = 20;
+}
+
+void Bground::draw() {
+
+}
--- src/header/Polygon.hpp
+++ src/header/Polygon.hpp
@@ -1,3 +1,11 @@
 #ifndef GRAFIKA_POLYGON_HPP
 #define GRAFIKA_POLYGON_HPP
+
+class Polygon {
+public:
+	Polygon();
+	~Polygon();
+	
+};
+
 #endif
