304fbc4e1e69b3fab317a1dbfa6620823f33deda
Ichlasul Amal <13511075@std.stei.itb.ac.id>
2014-03-31 15:51:22 +0700

    Draw tire:

--- src/ObsList.cpp
+++ src/ObsList.cpp
@@ -6,6 +6,9 @@ ObsList::ObsList() {
 
 	dRock[0] = Rock(Point(320, 120));
 	nRock = 1;
+
+	dTire[0] = Tire(Point(380, 300));
+	nTire = 1;
 }
 
 ObsList::~ObsList() {
@@ -21,5 +24,10 @@ void ObsList::draw() {
 	for (i = 0; i < nRock; i++) {
 		dRock[i].drawLine();
 		dRock[i].fillSmart();
+	}
+
+	for (i = 0; i < nTire; i++) {
+		dTire[i].drawLine();
+		dTire[i].fillSmart();
 	}
 }
