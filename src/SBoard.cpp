ef58ff2d3f72e0b0792fb0de1b622cbc6a668abe
Andreas Nugroho <andreasdwin@gmail.com>
2014-03-31 23:45:52 +0700

    updated SBoard

--- src/SBoard.cpp
+++ src/SBoard.cpp
@@ -3,27 +3,50 @@
 #include <graphics.h>
 #include <dos.h>
 #include <stdio.h>
+#include "src/header/Point.hpp"
+
+void ffill4(int x,int y,int fcolor,int ocolor)
+ {
+          int current;
+          current = getpixel(x,y);
+          
+          if(current == ocolor)
+          {
+               putpixel(x,y,fcolor);
+               
+                ffill4(x+1,y,fcolor,ocolor);
+               ffill4(x-1,y,fcolor,ocolor);
+               ffill4(x,y+1,fcolor,ocolor);
+               ffill4(x,y-1,fcolor,ocolor);
+               
+          }
+ }
 
 void SBoard::display(/*int score, */int win) {
-	int midx = getmaxx() / 2;
+    int midx = getmaxx() / 2;
     int midy = getmaxy() / 2;
     int y = 0;
-    for (int x = 0; x<(midx/2); x++) {
-        int left = midx-x;
-        int top = midy-y;
-        int right = midx+x;
-        int bottom = midy+y;
+    int left, top, right, bottom;
+    for (int x = 0; x<(midx/3); x++) {
+         left = midx-x;
+         top = midy-y;
+         right = midx+x;
+         bottom = midy+y;
         
-        cleardevice();
+        // cleardevice();
+         setcolor(WHITE);
         rectangle(left,top,right,bottom);
-        delay(5);
+        // ffill4(left+1,top+1,WHITE,0);
+        
+        delay(4);
         y++;
     }
     
+    
  //    char nbscore[255];
-	//  char* scoreLabel = "Score : ";
-	// sprintf(nbscore,"%s%d", scoreLabel,score);
-
+    //  char* scoreLabel = "Score : ";
+    // sprintf(nbscore,"%s%d", scoreLabel,score);
+    setcolor(BLACK);
     char textWin[255];
     char* isWinLabel = "Anda ";
     char* isWin;
@@ -33,7 +56,7 @@ void SBoard::display(/*int score, */int win) {
         isWin = "Kalah!";
     }
     sprintf(textWin,"%s%s", isWinLabel,isWin);
-	 
+     
      outtextxy((midx*3/4),(midy*3/4),textWin);
      // outtextxy((midx*3/4),midy,nbscore);
 }
